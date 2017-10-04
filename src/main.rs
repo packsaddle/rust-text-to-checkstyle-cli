11d9e6e3f92bc7501a61febb81a5ad6502ac7f9b
Sanemat <o.gata.ken@gmail.com>
2017-10-05 02:05:59 +0900

    (feat): like a cat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer).expect("can't read.");
+    println!("<xml>{}</xml>", buffer);
 }
