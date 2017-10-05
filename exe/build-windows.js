9ec879a9e878097cc4c32c2f2683699728dc6dff
Sanemat <o.gata.ken@gmail.com>
2017-10-06 01:25:57 +0900

    (chore): split build files

--- exe/build-windows.js
+++ exe/build-windows.js
@@ -2,10 +2,6 @@ const spawn = require('cross-spawn');
 const targets = [
   'i686-pc-windows-msvc',
   'x86_64-pc-windows-msvc',
-  'i686-unknown-linux-musl',
-  'x86_64-unknown-linux-musl',
-  'i686-apple-darwin',
-  'x86_64-apple-darwin',
 ];
 for (let target of targets) {
   spawn.sync('rustup', ['target', 'add', target], { stdio: 'inherit' });
