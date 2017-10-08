f957e724ae804324b3171cf234b7e42d69edf559
Sanemat <o.gata.ken@gmail.com>
2017-10-08 17:50:23 +0900

    feat(xml): build error piece

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,36 @@ fn main() {
     }
 }
 
+struct ErrorPiece {
+    column: u32,
+    line: u32,
+    message: String,
+    severity: String,
+    source: String,
+}
+
+impl ErrorPiece {
+    fn new(
+        column: u32,
+        line: u32,
+        message: &String,
+        severity: &String,
+        source: &String,
+    ) -> Result<ErrorPiece, &'static str> {
+        Ok(ErrorPiece {
+            column,
+            line,
+            message,
+            severity,
+            source,
+        })
+    }
+}
+
 //<?xml version='1.0'?>
 //<checkstyle>
 //    <file name='path/to/file'>
-//        <error column='0' line='0' message='valid text' severity='info' source='TextToCheckstyle'/>
+//        <error column='0' line='0' message='valid text'
+// severity='info' source='TextToCheckstyle'/>
 //    </file>
 //</checkstyle>
