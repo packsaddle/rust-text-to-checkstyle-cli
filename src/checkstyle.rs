8f3d740ff25d8450bb21e88de3a4a69e5d978811
Sanemat <o.gata.ken@gmail.com>
2017-10-09 13:23:14 +0900

    feat(checkstyle): error piece

--- src/checkstyle.rs
+++ src/checkstyle.rs
@@ -1,9 +1,9 @@
 extern crate quick_xml;
 
 use std::error::Error;
+
 use self::quick_xml::writer::Writer;
 use self::quick_xml::events::{BytesDecl, Event, BytesStart, BytesEnd, BytesText};
-
 
 pub struct ErrorPiece {
     column: u32,
@@ -11,6 +11,22 @@ pub struct ErrorPiece {
     message: String,
     severity: String,
     source: String,
+}
+
+impl ErrorPiece {
+    fn to_inner_xml(&self) -> Result<String, Box<Error>> {
+        let mut writer = Writer::new(Vec::new());
+        let error = b"error";
+        let mut element = BytesStart::borrowed(error, error.len());
+        element.push_attribute(("column".as_bytes(), self.column.to_string().as_bytes()));
+        element.push_attribute(("line".as_bytes(), self.line.to_string().as_bytes()));
+        element.push_attribute(("message".as_bytes(), self.message.as_bytes()));
+        element.push_attribute(("severity".as_bytes(), self.severity.as_bytes()));
+        element.push_attribute(("source".as_bytes(), self.source.as_bytes()));
+        writer.write_event(Event::Empty(element))?;
+        let result = writer.into_inner();
+        Ok(String::from_utf8(result).unwrap())
+    }
 }
 
 pub struct ErrorFile {
@@ -29,18 +45,15 @@ impl Container {
         writer.write_event(
             Event::Decl(BytesDecl::new(b"1.0", None, None)),
         )?;
-        writer.write_event(Event::Start(BytesStart::borrowed(
-            name,
-            name.len(),
-        )))?;
-        for error_file in &self.error_files {
-            writer.write_event(
-                Event::Text(BytesText::borrowed(error_file.name.as_bytes())),
-            )?;
-        }
         writer.write_event(
-            Event::End(BytesEnd::borrowed(name)),
+            Event::Start(BytesStart::borrowed(name, name.len())),
         )?;
+        for error_file in &self.error_files {
+            writer.write_event(Event::Text(
+                BytesText::borrowed(error_file.name.as_bytes()),
+            ))?;
+        }
+        writer.write_event(Event::End(BytesEnd::borrowed(name)))?;
         let result = writer.into_inner();
         Ok(String::from_utf8(result).expect("utf-8 output"))
     }
@@ -65,6 +78,25 @@ mod test {
             source: source.to_string(),
         };
         assert_eq!(piece.line, 2);
+    }
+    #[test]
+    fn piece_to_inner_xml() {
+        let column = 1;
+        let line = 2;
+        let message = "some message";
+        let severity = "info";
+        let source = "some checkstyle";
+        let piece = ErrorPiece {
+            column,
+            line,
+            message: message.to_string(),
+            severity: severity.to_string(),
+            source: source.to_string(),
+        };
+        let line1 = r#"<error column="1" line="2" message="some message" "#;
+        let line2 = r#"severity="info" source="some checkstyle"/>"#;
+        let expected = format!("{}{}", line1, line2);
+        assert_eq!(piece.to_inner_xml().unwrap(), expected);
     }
 
     #[test]
@@ -131,7 +163,10 @@ mod test {
             error_pieces: vec![piece],
         };
         let checkstyle = Container { error_files: vec![file] };
-        assert_eq!(checkstyle.to_xml().unwrap(), r#"<?xml version="1.0"?><checkstyle>path/to/file</checkstyle>"#);
+        assert_eq!(
+            checkstyle.to_xml().unwrap(),
+            r#"<?xml version="1.0"?><checkstyle>path/to/file</checkstyle>"#
+        );
     }
 }
 
